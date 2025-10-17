54d4d8eebb3c3e9b5c3352963a9d230933fd0e00
Davi Domingos de Oliveira <odomingosdavi@gmail.com>
2025-10-17 11:29:06 -0300

    Create HelloWorld.asm

---
+++ HelloWorld.asm
@@ -0,0 +1,17 @@
+section .data
+    msg db "Hello, World!", 0xA
+    len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov eax, 4          ; sys_write
+    mov ebx, 1          ; file descriptor (stdout)
+    mov ecx, msg        ; pointer to message
+    mov edx, len        ; message length
+    int 0x80            ; call kernel
+
+    mov eax, 1          ; sys_exit
+    xor ebx, ebx        ; exit code 0
+    int 0x80            ; call kernel
